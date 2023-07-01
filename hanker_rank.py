53e7edbae2d7580d399d13d4d3b9b54f5211a336
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-07-01 13:29:21 -0500

    Day 2 Exercise 3

--- hanker_rank.py
+++ hanker_rank.py
@@ -103,4 +103,20 @@ matrix = [[11, 2, 4],
           [4, 5, 6],
           [10, 8, -12]]
 
-print(diagonalDifference(matrix, 3))
+#print(diagonalDifference(matrix, 3)) #15
+
+#Exercise 3
+#Given a list of integers, count and return the number of times each value appears 
+# as an array of integers. Note: always return a frequency array with 100 elements, if want just number
+# in array is max()+1
+def countingSort(arr):
+    # Write your code here
+    freq_arr = [0]*100
+    
+    for num in arr:
+        freq_arr[num] += 1
+    
+    return freq_arr
+
+#print(countingSort([1,1,3,2,1])) #[0,3,1,1]
+#print(countingSort([63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33]))
