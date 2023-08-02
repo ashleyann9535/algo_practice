2f75a0ae997d16199769b3abb09f2eb3065f5a8e
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-08-01 21:09:44 -0500

    create phone number from array of 10 1-digit nums

--- codewars.py
+++ codewars.py
@@ -10,5 +10,31 @@ def max_multiple(divisor, bound):
     #Takes the Floor division then multiply it by the divisor
     return bound // divisor * divisor
 
-print(max_multiple(2,7)) #6
-print(max_multiple(10,50)) #50
+# print(max_multiple(2,7)) #6
+# print(max_multiple(10,50)) #50
+
+#Write a function that accepts an array of 10 integers (between 0 and 9), 
+# that returns a string of those numbers in the form of a phone number.
+def create_phone_number(n):
+    #First Attempt
+    # index = 0
+    # phone = '('
+    # for num in n:
+    #     if index == 2:
+    #         phone += str(num)
+    #         phone += ') '
+    #     elif index == 5:
+    #         phone += str(num)
+    #         phone += '-'
+    #     else:
+    #         phone += str(num)
+    #     index += 1
+
+    #Refactor
+    # *n unpacks the items in each array
+    phone = "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+    return phone
+
+
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) # "(123) 456-7890"
