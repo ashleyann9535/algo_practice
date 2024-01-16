d9d0cf8878dbb40f8f54f55d331ed8074a8714b1
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2024-01-15 20:21:10 -0600

    find lucky ticket

--- codeSignal.js
+++ codeSignal.js
@@ -23,4 +23,31 @@ const commonLetters = (s1, s2) => {
     return counter;
 }
 
-console.log(commonLetters("aabcc", "adcaa")); // 3
+//console.log(commonLetters("aabcc", "adcaa")); // 3
+
+//Ticket numbers usually consist of an even number of digits.
+//A ticket number is considered lucky if the sum of the first half of the digits is equal 
+//to the sum of the second half. Given a ticket number n, determine if it's lucky or not.
+const luckyTicket = n => {
+    const numStr = n.toString();
+
+    let middle = Math.floor(numStr.length / 2);
+
+    let part1 = numStr.substring(0, middle);
+    let part2 = numStr.substring(middle);
+
+    let partsArr = [part1, part2];
+
+    const digitSum = partsArr.map(str =>
+        str.split('').reduce((acc, curr) => acc + parseInt(curr), 0)
+    );
+    
+    if(digitSum[0] === digitSum[1]){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+console.log(luckyTicket(1230)); //True
+console.log(luckyTicket(239017)); //False
