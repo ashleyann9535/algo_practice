711399cc5cf01ae5e80ace1d2db000ca644027f6
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2024-01-15 19:46:01 -0600

    find common character count between strings

--- codeSignal.js
+++ codeSignal.js
@@ -1,11 +1,26 @@
 //Given an array of strings, return another array containing all of its longest strings.
-
 const longestStr = arr => {
     let sortArr = arr.sort((a,b) => b.length - a.length)
                     .filter(element => element.length === arr[0].length);
 
-    return sortArr
+    return sortArr;
+}
+//console.log(longestStr(["aba", "aa", "ad", "vcd", "aba"])); // ["aba", "vcd", "aba"]
+
+//Given two strings, find the number of common characters between them.
+const commonLetters = (s1, s2) => {
+    let counter = 0;
+    let arr1 = s1.split('');
+    let arr2 = s2.split('');
+
+    arr1.map(char1 => {
+        if(arr2.includes(char1)){
+            counter++;
+            arr2.splice(arr2.indexOf(char1), 1);
+        };
+    });
+
+    return counter;
 }
 
-console.log(longestStr(["aba", "aa", "ad", "vcd", "aba"])) // ["aba", "vcd", "aba"]
-
+console.log(commonLetters("aabcc", "adcaa")); // 3
